84f4c28139baed61f16493580bb9571152935e9c
Dozana <levan.soroznishvili@gmail.com>
2022-03-08 02:42:40 +0400

    onclick circle show different tab & content

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,40 @@
-console.log('redberry bootcamp');
+const prevStep = document.querySelector('.prev');
+const steps = document.querySelectorAll('.step');
+const nextStep = document.querySelector('.next');
+
+const stepsLength = steps.length;
+let currentStep = 1;
+
+nextStep.addEventListener('click', function() {
+    currentStep++;
+
+    if (currentStep > stepsLength) {
+        currentStep = stepsLength;
+    }
+
+    updateSteps();
+});
+
+prevStep.addEventListener('click', function()  {
+    currentStep--;
+
+    if (currentStep < 1) {
+        currentStep = 1;
+
+        return false;
+    }
+
+    updateSteps();
+});
+
+function updateSteps () {
+    for(let i = 1; i <= stepsLength; i++) {
+        ['step', 'tab','content'].forEach(item => {
+            if(i === currentStep) {
+                document.querySelector(`.${item}${i}`).classList.add('active');
+            } else {
+                document.querySelector(`.${item}${i}`).classList.remove('active');
+            }
+        });
+    }
+}
